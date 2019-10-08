e3cd1112aeef6c60015e28ceca87c6fb4cbf7421
gsebasscamarillo <54732141+gsebasscamarillo@users.noreply.github.com>
2019-10-07 22:56:46 -0500

    Creacion base de datos

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Segproy (models.Model):
+    rastreo = models.CharField(max_length=20, unique=True)
+    empresa = models.CharField(max_length=100)
+    proyecto = models.CharField(max_length=200)
+    estatus = models.CharField(max_length=200)
+    avance = models.IntegerField(max_length=3)
+    entrega = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.rastreo
+
+
